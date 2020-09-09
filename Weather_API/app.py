20da0c8c5ac94a3c2a025c0bb1b1936c259b56ba
ShadmanSahil <65828927+ShadmanSahil@users.noreply.github.com>
2020-09-10 04:27:23 +0600

    initialised sqlalchemy

--- Weather_API/app.py
+++ Weather_API/app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+
+db = SQLAlchemy(app)
