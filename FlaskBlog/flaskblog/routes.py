bb2dfeca501895ba328d10800ed1be45561cd9ac
ShadmanSahil <65828927+ShadmanSahil@users.noreply.github.com>
2020-09-10 03:57:41 +0600

    created two post routes
    one lets user create posts, validates the post and adds the post to database
    
    other lets user see a certain post using post id

--- FlaskBlog/flaskblog/routes.py
+++ FlaskBlog/flaskblog/routes.py
@@ -102,3 +102,23 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
