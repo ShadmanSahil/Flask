d244ecd44d29ee5402cb427d93ac9bcc96cdc1cd
ShadmanSahil <65828927+ShadmanSahil@users.noreply.github.com>
2020-09-10 03:20:00 +0600

    Initialised flask-bcrypt and flask-login
    flask-bcrypt will be used for hashing user passwords.
    
    flask-login will be used for managing user login sessions.

--- FlaskBlog/main.py
+++ FlaskBlog/main.py
@@ -2,9 +2,15 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
