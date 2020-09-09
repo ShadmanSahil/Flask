b1657d972d4bac8223001d1008e5f37f1420de34
ShadmanSahil <65828927+ShadmanSahil@users.noreply.github.com>
2020-09-10 03:22:57 +0600

    Update main.py

--- FlaskBlog/main.py
+++ FlaskBlog/main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -14,6 +14,10 @@ login_manager.login_message_category = 'info'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
