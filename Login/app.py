ad364e40da6bc89a78802813fd004550c976ac3d
Shadman Sahil <chowdhuryshadman52@gmail.com>
2020-09-10 15:55:17 +0600

    added a user class
    then added a few dummy users to a list
    
    used a list instead of an actual database as this is a microproject

--- Login/app.py
+++ Login/app.py
@@ -1,6 +1,21 @@
 from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = 'this is secret'
+
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='Anthony', password='password'))
+users.append(User(id=2, username='Becca', password='secret'))
+users.append(User(id=3, username='Carlos', password='somethingsimple'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
