d7c363d229b5b4a93fe1780284040c64b42a0553
Shadman Sahil <chowdhuryshadman52@gmail.com>
2020-09-10 16:00:17 +0600

    update login route
    validates data in form with the list containing dummy users

--- Login/app.py
+++ Login/app.py
@@ -19,6 +19,19 @@ users.append(User(id=3, username='Carlos', password='somethingsimple'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+
+        return redirect(url_for('login'))
+
     return render_template('login.html')
 
 @app.route('/profile')
