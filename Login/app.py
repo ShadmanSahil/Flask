6b7788f20a9034988eac6f277b67fc3de3086666
Shadman Sahil <chowdhuryshadman52@gmail.com>
2020-09-10 15:50:48 +0600

    added login route

--- Login/app.py
+++ Login/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
