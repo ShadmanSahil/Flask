983073cfd74b6ba738966429a0431b5468ee32ee
Shadman Sahil <chowdhuryshadman52@gmail.com>
2020-09-20 07:17:44 +0600

    add register and login route

--- Mail/main.py
+++ Mail/main.py
@@ -2,5 +2,13 @@ from flask import Flask
 
 app=Flask(__name__)
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    pass
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    pass
+
 if __name__=='__main__':
     app.run(debug=True)
