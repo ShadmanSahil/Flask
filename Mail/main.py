2d7d5e5ba32f9e97f0d66b2853746b42fa965c07
Shadman Sahil <chowdhuryshadman52@gmail.com>
2020-09-20 08:18:27 +0600

    initialised flask-login

--- Mail/main.py
+++ Mail/main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, Email
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin
 
 
 #configs 
@@ -14,6 +15,13 @@ db=SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 
 bcrypt=Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def User(user_id):
+    return User.query.get(user_id)
 
 
 #forms
@@ -72,6 +80,7 @@ def login():
     if user:
         password=form.password.data
         check=bcrypt.check_password_hash(user.password, password)
+       
 
 if __name__=='__main__':
     app.run(debug=True)
