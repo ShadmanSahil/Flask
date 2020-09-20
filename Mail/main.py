013b02b23757c907a428c304df84f264e5a827e1
Shadman Sahil <chowdhuryshadman52@gmail.com>
2020-09-20 07:45:06 +0600

    registration validators

--- Mail/main.py
+++ Mail/main.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Email
 
 app=Flask(__name__)
 
 class RegisterForm(FlaskForm):
-    username=StringField('Username')
-    email=StringField('Email')
-    password=PasswordField('Password')
-    confirm=PasswordField('Confirm Password')
+    username=StringField('Username', validators=[DataRequired(message='This cannot be empty')])
+    email=StringField('Email', validators=[DataRequired(message='This cannot be empty'), Email(message='Enter a valid email')])
+    password=PasswordField('Password', validators=[DataRequired(message='This cannot be empty')])
+    confirm=PasswordField('Confirm Password', validators=[DataRequired(message='This cannot be empty'), EqualTo(password, message='Passwords do not match')])
     submit=SubmitField('Register')
 
 @app.route('/register', methods=['GET','POST'])
